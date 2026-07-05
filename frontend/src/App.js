7900479d5c8a5e824a247ac15089e8f29f21442e
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-07-05 15:01:46 +0530

    update Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import "./App.css";
 import Header from "./component/layout/Header/Header.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router,Route } from "react-router-dom";
 import webFont from "webfontloader";
 import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/layout/Home/Home.js";
 
 function App() {
   useEffect(() => {
@@ -17,6 +18,7 @@ function App() {
   return (
     <Router>
       <Header />
+      <Route extact path="/" component={Home} />
       <Footer />
     </Router>
   );
