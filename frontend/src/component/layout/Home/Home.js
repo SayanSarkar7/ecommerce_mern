9c5a4e815c971ed3108252ce2174d4433c473ba0
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-07-07 19:51:36 +0530

    update Home page

--- frontend/src/component/layout/Home/Home.js
+++ frontend/src/component/layout/Home/Home.js
@@ -1,6 +1,14 @@
 import React, { Fragment } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css"
+import Product from "./Product.js"
+
+const product={
+  name:"blue shirt",
+  images:[{url:"https://unsplash.com/photos/woman-in-seashore-ohiyigDmlYc"}],
+  price:"₹599",
+  _id:"kjskakk_sayan"
+}
 
 const Home = () => {
   return (
@@ -15,6 +23,9 @@ const Home = () => {
         </a>
       </div>
       <h2 className="homeHeading">Featured Products</h2>
+      <div className="container" id="container">
+        <Product product={product}/>
+      </div>
     </Fragment>
   );
 };
---
+++ frontend/src/component/layout/Home/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactStars from "react-rating-stars-component";
+import { Link } from 'react-router-dom';
+
+const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
