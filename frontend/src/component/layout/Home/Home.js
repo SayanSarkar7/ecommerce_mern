a0232113024501ab678afd7c470ec3b1442a455b
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-07-08 20:26:07 +0530

    update Home page

--- frontend/src/component/layout/Home/Home.js
+++ frontend/src/component/layout/Home/Home.js
@@ -5,7 +5,7 @@ import Product from "./Product.js"
 
 const product={
   name:"blue shirt",
-  images:[{url:"https://unsplash.com/photos/woman-in-seashore-ohiyigDmlYc"}],
+  images:[{url:"https://images.unsplash.com/photo-1539008835657-9e8e9680c956?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}],
   price:"₹599",
   _id:"kjskakk_sayan"
 }
