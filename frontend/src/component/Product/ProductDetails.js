fd463a22bc41af550c5ac91ccd3961d3ce3db384
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-08-09 12:16:37 +0530

    update ProductDetails Page

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -31,6 +31,36 @@ const ProductDetails = ({ match }) => {
               ))}
           </Carousel>
         </div>
+        <div>
+          <div className="detailsBlock-1">
+            <h2>{product.name}</h2>
+            <p>Product # {product._id}</p>
+          </div>
+          <div className="detailsBlock-2">
+            <ReackStars {...options} />
+            <span>({product.numOfReviews} Reviews)</span>
+          </div>
+          <div className="detailsBlock-3">
+            <h1>{`₹${product.price}`}</h1>
+            <div className="detailsBlock-3-1">
+              <div className="detailsBlock-3-1-1">
+                <button>-</button>
+                <input type="number" value={1} />
+                <button>+</button>
+              </div>
+              <button>Add to Cart</button>
+            </div>
+            <p>
+              Status:
+              <b className={product.Stock<1? "redColor" : "greenColor"}>
+                {product.Stock < 1 ? "OutOfStock" : "InStock"}
+              </b>
+            </p>
+          </div>
+          <div className="detailsBlock-4">
+            
+          </div>
+        </div>
       </div>
     </Fragment>
   );
