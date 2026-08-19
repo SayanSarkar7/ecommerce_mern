9ff5e24d71ba6c848009008f1fdf23ac6db6fa19
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-08-19 15:21:17 +0530

    update login signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Home from "./component/Home/Home.js";
 import ProductDetails from "./component/Product/ProductDetails.js";
 import Products from "./component/Product/Products.js";
 import Search from "./component/Product/Search.js";
+import LoginSignUp from "./component/User/LoginSignUp.js";
 
 function App() {
   useEffect(() => {
@@ -26,6 +27,7 @@ function App() {
       <Route exact path="/products" component={Products} />
       <Route path="/products/:keyword" component={Products} />
       <Route exact path="/search" component={Search} />
+      <Route exact path="/login" component={LoginSignUp} />
       <Footer />
     </Router>
   );
