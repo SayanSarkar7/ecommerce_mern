66a55c5662da160aefa8668f071808c52727759c
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-09-03 14:30:41 +0530

    update profile icon page

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -5,9 +5,12 @@ import {
   CLEAR_ERRORS,
   REGISTER_USER_REQUEST,
   REGISTER_USER_SUCCESS,
-  REGISTER_USER_FAIL,LOAD_USER_REQUEST,
+  REGISTER_USER_FAIL,
+  LOAD_USER_REQUEST,
   LOAD_USER_SUCCESS,
   LOAD_USER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
 } from "../constants/userConstants";
 import axios from "axios";
 
@@ -54,10 +57,7 @@ export const loadUser = () => async (dispatch) => {
   try {
     dispatch({ type: LOAD_USER_REQUEST });
 
-    const { data } = await axios.get(
-      `/api/v1/me`,
-      
-    );
+    const { data } = await axios.get(`/api/v1/me`);
     dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
   } catch (error) {
     dispatch({
@@ -66,7 +66,18 @@ export const loadUser = () => async (dispatch) => {
     });
   }
 };
-
+// Logout User
+export const logout = () => async (dispatch) => {
+  try {
+    await axios.get(`/api/v1/logout`);
+    dispatch({ type: LOGOUT_SUCCESS });
+  } catch (error) {
+    dispatch({
+      type: LOGOUT_FAIL,
+      payload: error.response.data.errMsg,
+    });
+  }
+};
 
 // Clearing Errors
 export const clearError = () => async (dispatch) => {
